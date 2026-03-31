a3cec5d682085e85b69b26cb62908f2f9bef9695
Gabriel Carmona <gabriel.carmonat@sansano.usm.cl>
2026-04-01 00:05:58 +0200

    add specific la file

--- lib/matrix_gn/src/k2-tree/matrix.c
+++ lib/matrix_gn/src/k2-tree/matrix.c
@@ -1723,7 +1723,7 @@ uint64_t matInfo(matrix A, uint64_t size, uint64_t m, const char *fname, FILE *f
     printf("%" PRIu64 "\n", m);
     uint64_t nodes, leaves, nz;
     nodes = leaves = nz = 0;
-    uint32_t buffer = (uint32_t*) malloc(sizeof(uint32_t) * 2 * m);
+    uint32_t* buffer = (uint32_t*) malloc(sizeof(uint32_t) * 2 * m);
     nz = matCollect32Info(A, 0, (1ULL << A->logside) - 1, 0, (1ULL << A->logside) - 1, buffer, &nodes, &leaves);
     printf("%" PRIu64 "\n", m);
     myfree(buffer);
